50105cfd23e62c84bbbbff08a0b155c2318a3ebb
chenmengdi <1599126082@qq.com>
2017-10-12 13:27:04 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 陈孟迪 on 2017/10/12.
+//  Copyright © 2017年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by 陈孟迪 on 2017/10/12.
+//  Copyright © 2017年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
